d1198ec3b5e5ce119c6965d029abb8e7af5f8d5f
jorgefuentevil <95184005+jorgefuentevil@users.noreply.github.com>
2023-10-17 20:51:25 +0200

    Update echocon-udp-server.c

--- echocon-udp-server.c
+++ echocon-udp-server.c
@@ -8,31 +8,32 @@
 #include <errno.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+
 int main(int argc, char *argv[]){
  int npuerto=5,i,socketfd;
  char cadena[80]; 
  struct sockaddr_in myaddr,clientaddr;
  uint16_t puerto;
  socklen_t socksize;
- //Comprobamos que los argumentos son correctos
- if(argc>3){
-  fprintf(stderr,"El numero de argumentos es incorrecto\n");
+
+if(argc>3){
+  fprintf(stderr,"Invalid number of arguments\n");
   exit(0);
  }
  if(argc==3){
   if(strcmp(argv[1],"-p")!=0){
-    fprintf(stderr,"El unico parametro posible es -p\n");
+    fprintf(stderr,"Only possible argument is -p\n");
     exit(0);
   }
   npuerto=atoi(argv[2]);
  }
- //Convertimos el puerto y creamos el socket
+
  puerto=htons(npuerto);
  if((socketfd=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP))<0){
     perror("socket");
     exit(EXIT_FAILURE);
  }
- //Llenamos el struct address y bindeamos el socket en esta direccion
+
  myaddr.sin_family=AF_INET;
  myaddr.sin_port=puerto;
  myaddr.sin_addr.s_addr=INADDR_ANY;
@@ -43,15 +44,14 @@ int main(int argc, char *argv[]){
       exit(EXIT_FAILURE);
  }	
  while(1){
-  //Recibe la cadena
-  if(recvfrom(socketfd,cadena,80,0,(struct sockaddr*) &clientaddr,&socksize)<0){
+   if(recvfrom(socketfd,cadena,80,0,(struct sockaddr*) &clientaddr,&socksize)<0){
 	  perror("recvfrom");
 	  exit(EXIT_FAILURE);
-  }
-  printf("Recibido:%s\n",cadena);
-  fflush(stdout);
-  //Funcion que cambia mayusculas por minusculas
-  for(i=0;i<strlen(cadena);i++){
+   }
+   printf("Recived:%s\n",cadena);
+   fflush(stdout);
+   //Change uppercase to lowercase or vice versa
+   for(i=0;i<strlen(cadena);i++){
       if(cadena[i]>=97){
 	 cadena[i]=toupper(cadena[i]);
       }else{
@@ -60,13 +60,13 @@ int main(int argc, char *argv[]){
   }
   printf("Devolviendo: %s\n",cadena);
   fflush(stdout);
-  //Devolvemos la nueva cadena al cliente
+
   if(sendto(socketfd,cadena,80,0,(struct sockaddr*) &clientaddr,socksize)<0){
 	 perror("sendto()");
 	  exit(EXIT_FAILURE);
- }
- printf("El mensaje ha sido enviado\n");
- fflush(stdout);
+   }
+   printf("Message has been sent\n");
+   fflush(stdout);
  }
  return(0);
 }
